1f31e5f743a851b64ee49e206c263a96431d1665
rihab <128816556+rihabtl@users.noreply.github.com>
2023-10-25 12:32:58 +0200

    Delete 0x0A-python-inheritance directory

--- 0x0A-python-inheritance/2-is_same_class.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Checks if object is an instance of a class"""
-
-
-def is_same_class(obj, a_class):
-    """Checks if object is an instance of a class"""
-
-    return (type(obj) == a_class)
--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Module contains a function that returns true if object is an instance"""
-
-
-def is_kind_of_class(obj, a_class):
-    """This function returns true if object is an instance"""
-
-    return (isinstance(obj, a_class))
